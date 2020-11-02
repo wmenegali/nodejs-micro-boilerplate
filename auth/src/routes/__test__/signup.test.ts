f3950e2c05d408525ba0923073d4448046857e6c
Willian Menegali <wmenegali@gmail.com>
2020-11-02 10:42:17 -0300

    Tests setup and signup tests

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,53 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+const makeRequest = async (params: any, status?: number) => request(app).post('/api/users/signup').send(params).expect(status);
+
+it('returns a 201 on successful signup', async () =>
+  makeRequest({
+    email: 'test@test.com',
+    password: 'password'
+  }, 201));
+
+it('returns a 400 with invalid email', async () =>
+  makeRequest({
+    email: 'testtestasda',
+    password: 'password'
+  }, 400));
+
+it('returns a 400 with invalid password', async () =>
+  makeRequest({
+    email: 'testtestasda',
+    password: 'p'
+  }, 400));
+
+it('returns a 400 with missing email', async () =>
+  makeRequest({
+    password: 'password'
+  }, 400));
+
+it('returns a 400 with missing password', async () =>
+  makeRequest({
+    email: 'test@test.com',
+  }, 400));
+
+it('disallows duplicate emails', async () => {
+  await makeRequest({
+    email: 'test@test.com',
+    password: 'password'
+  }, 201);
+
+  await makeRequest({
+    email: 'test@test.com',
+    password: 'password'
+  }, 400);
+});
+
+it('sets a cookie after successful signup', async () => {
+  const response = await makeRequest({
+    email: 'test@test.com',
+    password: 'password'
+  }, 200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
